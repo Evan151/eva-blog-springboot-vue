15c22ba530258781407916e96303dc13043c753b
Evan Cun <evan.cun@ericsson.com>
2021-03-03 23:42:30 +0800

    init ui framewokr by vue-admin-template

---
+++ blog-ui/README.md
@@ -0,0 +1,6 @@
+
+## mock server 
+user : evan 
+pwd : cunhong
+
+https://mock.mengxuegu.com/project/60077611298655584170fbc2
---
+++ blog-ui/src/views/dashboard/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <h2>test msg : {{testMsg}}</h2>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import api from '@/api/test'
+
+export default {
+  name: 'Dashboard',
+  data(){
+    return{
+      testMsg:""
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  created(){
+    this.fetchData();
+  },
+  methods:{
+    fetchData() {
+      api.test().then(res=>{
+        this.testMsg= res.message;
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
